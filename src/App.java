f1980e5d51fcac76379991acc1351c74c20e9f2e
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-03-15 23:32:48 -0300

    Adição de comentários na classe App

--- src/App.java
+++ src/App.java
@@ -6,8 +6,10 @@ public class App {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
 
+        // Variáveis
         double xA, xB, xC, yA, yB, yC;
 
+        // Entradas
         System.out.println("Entre com os valores de X: ");
         xA = sc.nextDouble();
         xB = sc.nextDouble();
@@ -17,14 +19,17 @@ public class App {
         yB = sc.nextDouble();
         yC = sc.nextDouble();
 
+        // Cálculos
         double p = (xA + xB + xC) / 2.0;
         double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
         p = (yA + yB + yC) / 2.0;
         double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
 
+        // Saídas
         System.out.printf("Área do triângulo X: %.4f%n", areaX);
         System.out.printf("Área do triângulo Y: %.4f%n", areaY);
 
+        // Estrutura condicional if-else
         if (areaX > areaY) {
             System.out.println("Maior área: X");
         }
