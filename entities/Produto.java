b56871c9e3a80bab5e66b5069bb830eeec785edb
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-03-17 14:27:32 -0300

    Revisão dados do produto

--- entities/Produto.java
+++ entities/Produto.java
@@ -1,5 +1,23 @@
 package entities;
 
 public class Produto {
-    
+    public String nome;
+    public double preco;
+    public int quantidade;
+
+    public double ValorTotalEmEstoque() {
+        return preco * quantidade;
+    }
+
+    public void AdicionarProdutos(int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+    public void RemoverProdutos(int quantidade) {
+        this.quantidade -= quantidade;
+    }
+
+    public String toString() {
+        return "Nome: " + nome + ", preço: R$" + String.format("%.2f", preco) + ", quantidade: " + quantidade + ", valor total: R$" + String.format("%.2f", ValorTotalEmEstoque());
+    }
 }
